ab1c5a4d3a901000f497b27447363bd18b432359
thekyeZ <thekyez@gmail.com>
2026-07-14 14:17:07 +0200

    Add PWA manifest and service worker support
    - Register Angular service worker in production
    - Add web manifest and app icons
    - Update HTML metadata for installable app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import {
   LOCALE_ID,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePl from '@angular/common/locales/pl';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 registerLocaleData(localePl);
 
@@ -20,5 +22,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     { provide: LOCALE_ID, useValue: 'pl' },
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'PLN' },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
